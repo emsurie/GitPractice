46167aaf72488910930429ba9d57c9f18bfe7f97
Emily Suriel <emilysuriel0@gmail.com>
2025-07-09 10:38:48 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Indiana Suriel on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
